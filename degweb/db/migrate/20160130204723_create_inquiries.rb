ec3527dbfa576d893e502d05d5bf7509b21f01e4
adye20 <adye20@gmail.com>
2016-02-10 18:48:47 -0800

    added drop down lists

--- degweb/db/migrate/20160130204723_create_inquiries.rb
+++ degweb/db/migrate/20160130204723_create_inquiries.rb
@@ -7,7 +7,6 @@ class CreateInquiries < ActiveRecord::Migration
       t.string :address_1
       t.string :address_2
       t.string :city
-      t.string :state
       t.string :zip_code
       t.string :phone
       t.string :fax
