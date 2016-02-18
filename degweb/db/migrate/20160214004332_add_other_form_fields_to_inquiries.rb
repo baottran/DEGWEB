3c37649562af995707faa169f1ede843db1e91a5
adye20 <adye20@gmail.com>
2016-02-17 22:40:05 -0800

    fixed forms; logic based on inquiry type and other vechicle works; logic for area of vechicle for each inquiry type is next

--- degweb/db/migrate/20160214004332_add_other_form_fields_to_inquiries.rb
+++
@@ -1,6 +0,0 @@
-class AddOtherFormFieldsToInquiries < ActiveRecord::Migration
-  def change
-    add_column :inquiries, :make_other_field, :string
-    add_column :inquiries, :area_of_vechicle_other_field, :string
-  end
-end
