c733a323c118565ff17e9b3ecf07d012dd26132e
adye20 <adye20@gmail.com>
2016-03-09 19:09:07 -0800

    deploying to heroku

--- degweb/db/schema.rb
+++ degweb/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160303063430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160303063430) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["inquiry_id"], name: "index_comments_on_inquiry_id"
+  add_index "comments", ["inquiry_id"], name: "index_comments_on_inquiry_id", using: :btree
 
   create_table "inquiries", force: :cascade do |t|
     t.string   "name"
@@ -116,6 +119,7 @@ ActiveRecord::Schema.define(version: 20160303063430) do
     t.string   "remember_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "comments", "inquiries"
 end
