d2e3eb0e2bfb666ce38286a9cc7e5d90c5a11080
Bao Tran <baottran>
2016-04-04 14:56:22 -0700

    revert to 4e1bb

--- db/migrate/20160223080110_add_attachment_to_inquiries.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentToInquiries < ActiveRecord::Migration
-  def self.up
-    add_attachment :inquiries, :attachment
-  end
-
-  def self.down
-    remove_attachment :inquiries, :attachment
-  end
-end
--- test/mailers/previews/inquiry_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/inquiry_mailer
-class InquiryMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/inquiry_mailer/new_inquiry
-  def new_inquiry
-    InquiryMailer.new_inquiry
-  end
-
-end
