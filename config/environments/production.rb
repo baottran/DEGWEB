926c7892a51cb1ed4b753ba70f24cc3bd82ccd4a
Bao Tran <baottran>
2016-04-25 00:38:07 -0700

    fixed product database missing ]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     :s3_credentials => {
       :region => 'us-west-1',
       :bucket => ENV["aws_bucket"],
-      :access_key_id => ENV["aws_access_key_id",
+      :access_key_id => ENV["aws_access_key_id"],
       :secret_access_key => ENV["aws_secret_access_key"]
     }
   }
