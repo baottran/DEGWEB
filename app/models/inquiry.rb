d042380c99e6e7ee34560a8fe786a78e9c887ed2
Bao Tran <bao.tran@macys.com>
2016-09-21 21:01:06 -0700

    fix file path and customer data

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -18,31 +18,31 @@ class Inquiry < ActiveRecord::Base
   has_attached_file :attachment,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :path => "/inquiries/:id-1", 
+                    :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment2,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :path => "/inquiries/:id-2", 
+                    :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment3,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :path => "/inquiries/:id-3", 
+                    :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment4,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :path => "/inquiries/:id-4", 
+                    :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment5,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :path => "/inquiries/:id-5", 
+                    :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"               
 
   after_initialize :init, :set_criteria, :set_area_of_vehicle
