0179b37e4ce198db86f1a0a4c4a5552a4346b849
Bao Tran <bao.tran@macys.com>
2016-10-03 19:18:12 -0700

    added filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   include SessionsHelper
-  skip_before_filter  :verify_authenticity_token
+   skip_before_action :verify_authenticity_token
 
   def new
   	@user = User.new 
