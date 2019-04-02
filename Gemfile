dd772378b95d2a3f5b00dec461e0efa050f724fa
chezze911 <chinguyen@chis-mbp.lan>
2019-04-02 09:39:10 -0700

    Update dependencies and fix update bugs

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
-ruby "2.3.0"
+ruby "2.6.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.7'
+gem 'rails', '5.0.2'
 
 group :production do
   gem 'pg', '0.20.0'
@@ -94,6 +94,14 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  #guard automates various tasks by running custom rules whenever
+  # file or directories are modified.
+  gem 'guard-bundler', require: false
+
+  #testing plugin
+  gem 'guard-rspec', require: false
+
 end
 
 gem "recaptcha", require: "recaptcha/rails"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,11 @@ module ApplicationHelper
 		# link_to title, {:sort => column, :direction => direction}, :class => 'current'
 		# link_to title, {sort: column, direction: direction}, class: css_class
 
-		link_to title, params.merge(:sort => column, :direction => direction, :status => status, :page => nil), class: css_class
+		# link_to title, params.merge(:sort => column, :direction => direction, :status => status, :page => nil), class: css_class
+		# received the following error: ArgumentError in Inquiries#index
+		# Showing /Users/chinguyen/Desktop/DEGWEB/app/views/inquiries/index.html.erb where line #174 raised:
+		#
+		# Attempting to generate a URL from non-sanitized request parameters! An attacker can inject malicious data into the generated URL, such as changing the host. Whitelist and sanitize passed parameters to be secure.
+		link_to title, params.permit(:direction, :page).merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
 	end
 end
