3626f45e9f5993c4cd35c0bb507593619e1f005c
Chi V Nguyen <chinguyen2018@gmail.com>
2019-04-17 18:48:26 -0700

    update s3

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -22,40 +22,30 @@ class Inquiry < ActiveRecord::Base
   has_attached_file :attachment,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :s3_region: ENV["aws_region"],
-                    :s3_protocol => 'https',
                     :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment2,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :s3_region: ENV["aws_region"],
-                    :s3_protocol => 'https',
                     :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment3,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :s3_region: ENV["aws_region"],
-                    :s3_protocol => 'https',
                     :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment4,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :s3_region: ENV["aws_region"],
-                    :s3_protocol => 'https',
                     :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"
 
   has_attached_file :attachment5,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-                    :s3_region: ENV["aws_region"],
-                    :s3_protocol => 'https',
                     :path => "/inquiries/:id/:basename.:extension", 
                     :url => ":s3_domain_url"               
 
